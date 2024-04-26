293d9407478e37ddfb278924ac6e46e366b86c48
MinhLong2410-02 <minhlong2002@gmail.com>
2024-04-26 22:24:07 +0700

    update migrate files for windows and linux

---
+++ iuhkart/migrate.bat
@@ -0,0 +1,7 @@
+@echo off
+del /s /q apps\customers\migrations\0001_initial.py
+del /s /q apps\vendor\migrations\0001_initial.py
+
+del db.sqlite3
+python manage.py makemigrations
+python manage.py migrate
--- iuhkart/migrate.sh
+++ iuhkart/migrate.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+rm -rf apps/customers/migrations/0001_initial.py
+rm -rf apps/vendor/migrations/0001_initial.py
+
+rm -f db.sqlite3
 python3 manage.py makemigrations
 
 python3 manage.py migrate
