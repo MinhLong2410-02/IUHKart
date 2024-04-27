14883f89932e72f1c72892ca13c7f1648efd8a0b
MinhLong2410-02 <minhlong2002@gmail.com>
2024-04-27 09:09:08 +0700

    build: add model ProductCategory

--- iuhkart/apps/product/models.py
+++ iuhkart/apps/product/models.py
@@ -8,6 +8,13 @@ class Category(models.Model):
     category_name = models.CharField(max_length=50)
     slug = AutoSlugField(max_length=100, populate_from='category_name')
     category_img_url = models.URLField(max_length=100)
+    def __str__(self):
+        return f"{self.category_id} - {self.category_name} - {self.category_img_url}"
+
+class CategoryProduct(models.Model):
+    category_product_id = models.AutoField(primary_key=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
 
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
@@ -21,6 +28,11 @@ class Product(models.Model):
     
     created_by = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, related_name="customer", on_delete=models.CASCADE, null=True)
+    category = models.ManyToManyField(Category, through=CategoryProduct)
+    
+    def __str__(self) -> str:
+        return f"{self.product_id} - {self.product_name} - {self.original_price} - {self.stock} - {self.brand} - {self.created_by}"
+    
     class Meta:
         db_table = 'product'
         verbose_name_plural = 'Products'
