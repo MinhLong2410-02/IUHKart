a551d920300cbc0d08ab2ab68c7d07988151c085
MinhLong2410-02 <minhlong2002@gmail.com>
2024-05-11 14:30:31 +0700

    Build: add verified

--- iuhkart/apps/vendor/urls.py
+++ iuhkart/apps/vendor/urls.py
@@ -3,7 +3,8 @@
 # URL configuration for vendor app.
 # """
 from django.urls import path
-from .views import *
+from django.contrib.auth import views as auth_views
+from apps.vendor.views import *
 app_name = 'vendor'
 
 urlpatterns = [
@@ -11,4 +12,11 @@ urlpatterns = [
     path('sign-in/', SignInView.as_view(), name='vendor_sign_in'),
     path('add-product/', SignInView.as_view(), name='add_product'),
     path('sign-up/', VendorSignUpView, name='vendor_sign_up'),
+    path(
+        "sign-out",
+        auth_views.LogoutView.as_view(template_name="sign_out.html"),
+        name="vendor_sign_out",
+    ),
+    path('add-product', product_views.create_product_view, name='add_product'),
+    path('edit/<int:product_id>', product_views.edit_product_view, name='edit_product'),
 ]
