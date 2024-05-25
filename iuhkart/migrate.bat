8991e8407b9780d912f9a60794b1ff51177e0c78
MinhLong2410-02 <minhlong2002@gmail.com>
2024-05-26 02:22:01 +0700

    Build: add cart API

--- iuhkart/migrate.bat
+++ iuhkart/migrate.bat
@@ -2,6 +2,7 @@
 del /s /q apps\account\migrations\0001_initial.py
 del /s /q apps\product\migrations\0001_initial.py
 del /s /q apps\address\migrations\0001_initial.py
+del /s /q apps\cart\migrations\0001_initial.py
 
 del db.sqlite3
 python manage.py makemigrations
--- iuhkart/migrate.sh
+++ iuhkart/migrate.sh
@@ -1,6 +1,7 @@
 rm -rf apps/account/migrations/0001_initial.py
 rm -rf apps/product/migrations/0001_initial.py
 rm -rf apps/address/migrations/0001_initial.py
+rm -rf apps/cart/migrations/0001_initial.py
 
 rm -f db.sqlite3
 python3 manage.py makemigrations
