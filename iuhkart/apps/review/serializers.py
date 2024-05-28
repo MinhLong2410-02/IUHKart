7a59b285bf17a3665205a71fcd2ea3cde89aec1c
MinhLong2410-02 <minhlong2002@gmail.com>
2024-05-29 05:23:11 +0700

    Build: add pagination

--- iuhkart/apps/review/serializers.py
+++ iuhkart/apps/review/serializers.py
@@ -5,3 +5,13 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ['review_id', 'review_rating', 'review_content', 'review_date', 'customer']
+
+class CustomerReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['review_id', 'product', 'review_rating', 'review_date', 'review_content']
+        read_only_fields = ['review_date']  # Auto-set fields should not be editable
+
+    def create(self, validated_data):
+        # Create the Review instance
+        return Review.objects.create(**validated_data)
