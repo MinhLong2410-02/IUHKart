0b1d133614b1672fabbf809c5e753257db6a0543
MinhLong2410-02 <minhlong2002@gmail.com>
2024-05-30 21:59:40 +0700

    Add redis

--- iuhkart/apps/discount/models.py
+++ iuhkart/apps/discount/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Discount(models.Model):
     discount_id = models.AutoField(primary_key=True)
-    # vendor = models.ForeignKey('account.Vendor', on_delete=models.CASCADE, db_column='vendor_id')
+    vendor = models.ForeignKey('account.Vendor', on_delete=models.CASCADE, db_column='vendor_id')
     product = models.ForeignKey('product.Product', on_delete=models.CASCADE, db_column='product_id')
     name = models.CharField(max_length=100)
     discount_percent = models.DecimalField(max_digits=5, decimal_places=2)
