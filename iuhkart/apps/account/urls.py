7c5fd3fc83c87eb953ff93a3d4caf2a2dd2e5c83
Colosck-alternative <chaule222a@gmail.com>
2024-10-20 16:29:09 +0700

    add / to url

--- iuhkart/apps/account/urls.py
+++ iuhkart/apps/account/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('api/register/customer/', RegisterCustomerView.as_view(), name='register_customer'),
     path('api/vendor/me', VendorDetailView.as_view(), name='vendor-detail'),
     path('api/register/vendor/', RegisterVendorView.as_view(), name='register_vendor'),
-    path('api/update-image/customer', UpdateCustomerAvatarView.as_view(), name='update-img-customer'),
-    path('api/update-image/vendor', UpdateVendorLogoView.as_view(), name='update-img-vendor'),
-    path('api/bank-account/vendor', UpdateBankAccountView.as_view(), name='update-bank-account'),
-    path('api/update-dob/customer', CustomerUpdateAPIView.as_view(), name='update-dob-customer')
+    path('api/update-image/customer/', UpdateCustomerAvatarView.as_view(), name='update-img-customer'),
+    path('api/update-image/vendor/', UpdateVendorLogoView.as_view(), name='update-img-vendor'),
+    path('api/bank-account/vendor/', UpdateBankAccountView.as_view(), name='update-bank-account'),
+    path('api/update-dob/customer/', CustomerUpdateAPIView.as_view(), name='update-dob-customer')
 ]
