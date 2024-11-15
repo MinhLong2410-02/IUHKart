58b757e763a409d92e952a9ed1e43e822f3e9198
MinhLong2410-02 <minhlong2002@gmail.com>
2024-11-15 19:56:12 +0700

    delete metabase

--- web/iuhkart/apps/metabase/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from apps.metabase.views import MetabaseDashboardView
-urlpatterns = [
-    path('api/vendor/dashboard', MetabaseDashboardView.as_view(), name='vendor-summary-dashboard')
-]
