50f131283c0eefc7ad7d0df49e098d97d5df3567
qxnam <quachnam311@gmail.com>
2024-11-26 17:00:33 +0000

    update fake order data

--- web/schema/gen.py
+++ web/schema/gen.py
@@ -1,6 +1,6 @@
 import psycopg2
 import pandas as pd
-import random
+import random, os
 
 # # Thông tin kết nối tới PostgreSQL
 # conn_info = {
@@ -78,14 +78,15 @@ import random
 # df_p.to_csv('./data/discounts.csv', index=False)
 
 def _get_price(product_id):
-    df_p = pd.read_csv('./data/products.csv')
-    df_d = pd.read_csv('./data/discounts.csv')
+    df_p = pd.read_csv('./Database/products.csv')
+    df_d = pd.read_csv('./Database/discounts.csv')
+    df_pd = pd.read_csv('./Database/product_discount.csv')
     price = df_p[df_p['product_id'] == product_id]['original_price'].values[0]
-    discount = df_d[df_d['product_id'] == product_id]
-    if len(discount) == 0:
+    discount_id = df_pd[df_pd['product_id'] == product_id]
+    if len(discount_id) == 0:
         return price
-    if discount['in_use'].values[0] == False:
-        return price
+    discount_id = discount_id['discount_id'].values[0]
+    discount = df_d[df_d['discount_id'] == discount_id]
     discount = discount['discount_percent'].values[0]
     return price * (1 - discount / 100)
 
@@ -98,47 +99,41 @@ def _get_random_date(start_date=None):
     return random_date.strftime('%Y-%m-%d')
 
 def gen_order(n=1):
-    df_p = pd.read_csv('./data/products.csv')
-    df_o = pd.read_csv('./data/orders.csv')
-    df_op = pd.read_csv('./data/order_products.csv')
-    df_t = pd.read_csv('./data/transactions.csv')
-    o_id = len(df_o)
-    op_id = len(df_op)
-    t_id = len(df_t)
+    df_p = pd.read_csv('./Database/products.csv')
+    o_id = 0
+    if os.path.exists('./Database/orders.csv'):
+        df_o = pd.read_csv('./Database/orders.csv')
+        o_id = len(df_o)
     new_orders = []
     new_order_products = []
     new_transactions = []
-    for i in range(n):
-        n_product = random.randint(1, 3)
-        product_ids = random.choices(df_p['product_id'], k=n_product)
+    for _ in range(n):
+        product_id = random.choices(df_p['product_id'], k=1)[0]
         total_price = 0
-        for product_id in product_ids:
-            quantity = random.randint(1, 3)
-            price = _get_price(product_id)
-            new_order_products.append([op_id, quantity, price, o_id, product_id])
-            total_price += price * quantity
-            op_id += 1
+        quantity = random.randint(1, 3)
+        price = _get_price(product_id)
+        new_order_products.append([o_id, quantity, price, o_id, product_id])
+        total_price += price * quantity
 
         # order_id,order_number,shipping_date,order_date,order_status,order_total,total_price,address_id,customer_id
         start_date = _get_random_date()
         shipping_date = _get_random_date(start_date)
         status = random.choices(['completed', 'completed', 'completed', 'completed', 'completed', 'completed', 'cancelled'])[0]
-        new_orders.append([o_id, o_id, start_date, shipping_date, status, n_product, total_price, random.randint(1, 4), random.randint(1, 3)])
+        new_orders.append([o_id, o_id, start_date, shipping_date, status, 1, total_price, random.randint(1, 4), random.randint(1, 3)])
         
         # transaction_id,transation_date,total_money,status,customer_id,order_id
         status = 'completed' if status == 'completed' else 'failed'
 
-        new_transactions.append([t_id, _get_random_date(shipping_date), total_price, status, random.randint(1, 4), o_id])
-        t_id += 1
+        new_transactions.append([o_id, _get_random_date(shipping_date), total_price, status, random.randint(1, 4), o_id])
         o_id += 1
 
     df_orders = pd.DataFrame(new_orders, columns=['order_id', 'order_number', 'shipping_date', 'order_date', 'order_status', 'order_total', 'total_price', 'address_id', 'customer_id'])
     df_order_products = pd.DataFrame(new_order_products, columns=['order_product_id', 'quantity', 'price', 'order_id', 'product_id'])
     df_transactions = pd.DataFrame(new_transactions, columns=['transaction_id', 'transaction_date', 'total_money', 'status', 'customer_id', 'order_id'])
     
-    df_orders.to_csv('./data/orders.csv', index=False, mode='a', header=False)
-    df_order_products.to_csv('./data/order_products.csv', index=False, mode='a', header=False)
-    df_transactions.to_csv('./data/transactions.csv', index=False, mode='a', header=False)
+    df_orders.to_csv('./Database/orders.csv', index=False, mode='a', header=False if os.path.exists('./Database/orders.csv') else True)
+    df_order_products.to_csv('./Database/order_products.csv', index=False, mode='a', header=False if os.path.exists('./Database/order_products.csv') else True)
+    df_transactions.to_csv('./Database/transactions.csv', index=False, mode='a', header=False if os.path.exists('./Database/transactions.csv') else True)
     
 if __name__ == "__main__":
     gen_order(n=1000)
