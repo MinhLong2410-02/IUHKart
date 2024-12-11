a016a648896bcfbf44d11002cc9762639667fc80
MinhLong2410-02 <minhlong2002@gmail.com>
2024-12-12 01:12:19 +0700

    fix django and tracking api

--- web/iuhkart/apps/account/serializers.py
+++ web/iuhkart/apps/account/serializers.py
@@ -77,7 +77,7 @@ class CustomerSerializer(serializers.ModelSerializer):
 
 class CustomerSerializer(serializers.ModelSerializer):
     user = UserSerializer()
-    date_of_birth = serializers.DateField(required=False, allow_null=True)
+    date_of_birth = serializers.DateTimeField(required=False, allow_null=True)
 
     class Meta:
         model = Customer
